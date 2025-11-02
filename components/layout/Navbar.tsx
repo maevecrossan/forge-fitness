7059f026d8e3c89d41ab636ce48abc719e58ea3c
Maeve Crossan <maevecrossan@gmail.com>
2025-11-02 16:16:44 +0000

    Update to be full width.

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -3,7 +3,7 @@
 export default function Navbar() {
 	return (
 		<nav className="bg-zinc-800 p-4">
-			<div className="container mx-auto flex justify-between items-center">
+			<div className="w-full flex justify-between items-center">
 				<h1 className="text-white text-2xl">Forge</h1>
 				<ul className="flex space-x-4">
 					<li>

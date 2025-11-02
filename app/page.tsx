c2898c199bd9d30c9ff5bfc409dd60e008d9566b
Maeve Crossan <maevecrossan@gmail.com>
2025-11-02 14:54:16 +0000

    Invert h1 and bg colours.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
 
-// Hoeme page component
+// Home page component
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-
-      </main>
-    </div>
-  );
+	return (
+		<div className="flex min-h-screen items-center justify-center bg-zinc-800 font-sans dark:bg-black">
+			<main className="flex min-h-screen w-full flex-col items-center justify-between py-32 px-16 dark:bg-black sm:items-start">
+				<h1 className="text-5xl font-extrabold text-white dark:text-white sm:text-6xl">
+					Forge Fitness
+				</h1>
+			</main>
+		</div>
+	);
 }

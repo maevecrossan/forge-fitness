e50cc15d8de7b7dcc8a69971e756b227daad8e74
Maeve Crossan <maevecrossan@gmail.com>
2025-11-02 15:52:55 +0000

    Add basic navbar.

---
+++ components/layout/Navbar.tsx
@@ -0,0 +1,28 @@
+// Responsive Navbar component
+
+export default function Navbar() {
+	return (
+		<nav className="bg-zinc-800 p-4">
+			<div className="container mx-auto flex justify-between items-center">
+				<h1 className="text-white text-2xl">Forge Fitness</h1>
+				<ul className="flex space-x-4">
+					<li>
+						<a href="#" className="text-white hover:underline">
+							Home
+						</a>
+					</li>
+					<li>
+						<a href="#" className="text-white hover:underline">
+							About
+						</a>
+					</li>
+					<li>
+						<a href="#" className="text-white hover:underline">
+							Contact
+						</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
+}

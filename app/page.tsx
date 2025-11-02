1ea0a0ae0f50d84f84566856dd9c586c4df5b007
Maeve Crossan <maevecrossan@gmail.com>
2025-11-02 15:12:48 +0000

    Add aurora background home page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
 import Image from "next/image";
+import { AuroraBackground } from '@/components/ui/shadcn-io/aurora-background';
 
 // Home page component
 
 export default function Home() {
 	return (
 		<div className="flex min-h-screen items-center justify-center bg-zinc-800 font-sans dark:bg-black">
-			<main className="flex min-h-screen w-full flex-col items-center justify-between py-32 px-16 dark:bg-black sm:items-start">
-				<h1 className="text-5xl font-extrabold text-white dark:text-white sm:text-6xl">
-					Forge Fitness
-				</h1>
-			</main>
+			<AuroraBackground>
+				<main className="flex min-h-screen w-full flex-col items-center justify-between py-32 px-16 dark:bg-black sm:items-start">
+					<h1 className="text-5xl font-extrabold text-white dark:text-white sm:text-6xl">
+						Forge Fitness
+					</h1>
+				</main>
+			</AuroraBackground>
 		</div>
 	);
 }
